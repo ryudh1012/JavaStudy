7c558f17cb0170b0e10dbce0e25162159bdfacc8
ryudh1012 <ryudh1012@gmail.com>
2024-04-08 11:50:05 +0900

    source

--- src/main/java/Study1.java
+++ src/main/java/Study1.java
@@ -14,8 +14,9 @@ public class Study1 {
             System.out.println("a가 b보다 큽니다");
         } else if (a == b) {
             System.out.println("a와 b는 같습니다.");
-        } else if (a < b) {
+        } else {
             System.out.println("a가 b보다 작습니다");
         }
+        s.close();
     }
 }
