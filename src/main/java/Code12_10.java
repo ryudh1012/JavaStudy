af4a37a7363c088129507022af7a6d435c057774
ryudh1012 <ryudh1012@gmail.com>
2024-06-19 08:51:40 +0900

    source

---
+++ src/main/java/Code12_10.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Code12_10 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("BorderLayout 연습");
+
+        frame.setLayout(new BorderLayout(5, 5));
+
+        JButton btn1 = new JButton("버튼1");
+        frame.add(btn1, BorderLayout.NORTH);
+        JButton btn2 = new JButton("버튼2");
+        frame.add(btn2, BorderLayout.WEST);
+        JButton btn3 = new JButton("버튼3");
+        frame.add(btn3, BorderLayout.CENTER);
+        JButton btn4 = new JButton("버튼4");
+        frame.add(btn4, BorderLayout.EAST);
+        JButton btn5 = new JButton("버튼5");
+        frame.add(btn5, BorderLayout.SOUTH);
+
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+    }
+}
